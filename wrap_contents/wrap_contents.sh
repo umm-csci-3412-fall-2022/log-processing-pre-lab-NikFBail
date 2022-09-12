c21625465eeae05bd3651df4e9d1b68d2ffe4ab5
Nik Bailey <bail0301@morris.umn.edu>
2022-09-12 08:53:32 -0500

    Wrote code for wrap_contents.sh

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,3 +1,8 @@
 #! usr/bin/sh
-pwd
-ls
+# automate wrapping content
+# takes 3 arguments:
+# name of file to be wrapped
+# name used to specify the header and footer
+# name of resulting file
+
+cat "$2"_header.html "$1" "$2"_footer.html > "$3"
