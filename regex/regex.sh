e66308c04115af73dfc4e9d40f9cae5f3dd1351e
Nik Bailey <bail0301@morris.umn.edu>
2022-09-13 09:07:57 -0500

    Finished lab, completed regex tasks

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,5 +1,23 @@
-#1 /usr/bin/bash
+#!/usr/bin/bash
 # write regex to extract desired content
 # and display it in the preferred way
 
-sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt
+#$0 means the whole line, followed by the regular expression within /   /
+#Finds any alphanumeric character(one or more) as the first part
+#Finds any alphanumeric character(one or more) as the second part
+#Puts both parts into an array groups
+#Then print each part of groups
+#Using contents of the r0_input.txt and moving into r0_output.txt
+awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r0_input.txt > r0_output.txt
+
+#Should take the name and favorite snadwich type and put them in the array
+#Print them out in a numbered list
+#Takes a capital letter for the name
+awk 'match($0, /([A-Z]\w+).+is ([hat]\w+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r1_input.txt > r1_output.txt
+
+#Takes a food order and if its for here or to go
+#Takes a word followed by a period, possibly more than once for the first argument
+#Capture whats after 'with'
+#\S+ capture as many non whitespace characters in a row as possible
+# < is from text file, > is put into text file
+awk 'match($0, /with (\S+) (.+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n"}' r2_input.txt > r2_output.txt
