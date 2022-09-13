dfa474beb983ead16ec5b79b54b89830620484a4
Nik Bailey <bail0301@morris.umn.edu>
2022-09-13 09:20:37 -0500

    Committing changes
    Using gitKraken to commit changes because I think I messed something up, hopefully this will get the checks to pass

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,8 +1,9 @@
-#! usr/bin/sh
+#! /usr/bin/bash
 # automate wrapping content
 # takes 3 arguments:
-# name of file to be wrapped
-# name used to specify the header and footer
-# name of resulting file
+# name of file to be wrapped ($1)
+# name used to specify the header and footer ($2)
+# name of resulting file ($3)
+# use cat to concatenate files, > to move into a new file
 
-cat "$2"_header.html "$1" "$2"_footer.html > "$3"
+cat "${2}"_header.html "$1" "${2}"_footer.html > "$3"
